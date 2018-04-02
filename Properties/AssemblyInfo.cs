61369aff1835cd1ba4ed5cd205cc974ae0ab821e
Richard MacAleese <macaleese.1@gmail.com>
2018-04-01 23:28:59 -0400

    Updated assembly description.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SpawnLocationChatCommand")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Adds the /spawnloc command to Terraria. Requires TModLoader.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SpawnLocationChatCommand")]
